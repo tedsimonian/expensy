a22c0c57baaf9bd5a1f625e172b3ae871857dfae
Ted <tedsimonian@gmail.com>
2019-03-25 14:15:34 -0700

    testing webpack changes

--- src/App.js
+++ src/App.js
@@ -12,10 +12,6 @@ import "react-dates/lib/css/_datepicker.css";
 import { firebase } from "./firebase/firebase";
 import LoadingPage from "./components/LoadingPage";
 
-if (!global._babelPolyfill) {
-  require("babel-polyfill");
-}
-
 const store = configureStore();
 
 const jsx = (
