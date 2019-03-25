b8f482bc29ba1990781d246f5e7de3f5a71a2388
Ted <tedsimonian@gmail.com>
2019-03-25 15:31:32 -0700

    adding continuous integration

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ const renderApp = () => {
 
 ReactDOM.render(<LoadingPage />, document.getElementById("app"));
 
-firebase.auth().onAuthStateChanged((user) => {
+firebase.auth().onAuthStateChanged(user => {
   if (user) {
     store.dispatch(login(user.uid));
     store.dispatch(startSetExpenses()).then(() => {
