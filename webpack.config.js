0afa01b823325cb717fd5cb524c7d602db2eb061
Ted <tedsimonian@gmail.com>
2019-03-25 14:08:50 -0700

    testing webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = env => {
 
   return {
     mode: isProduction ? "production" : "development",
-    entry: "./src/app.js",
+    entry: ["./src/app.js"],
     output: {
       path: path.join(__dirname, "public", "dist"),
       filename: "bundle.js"
